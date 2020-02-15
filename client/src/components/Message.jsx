2d833bcf398b5e9e3a6010b3dfc622011453488e
neel9248 <neeltrivedi10@gmail.com>
2020-02-15 09:05:58 -0500

    yo

--- client/src/components/Message.jsx
+++ client/src/components/Message.jsx
@@ -1,10 +1,6 @@
 import React, {useState} from 'react';
 
 import './Message.css';
-<<<<<<< HEAD
-=======
-//const socket = io.connect('http://localhost:5000');
->>>>>>> ced8e831ecd20c15338cfe9ce543098ee7007d05
 
 export default function Message(props) {
     return (
