ef99e4dd0e1a395da3e7a5ca67c922c6172d8e1b
neel9248 <neeltrivedi10@gmail.com>
2020-02-14 21:40:55 -0500

    connected socket.io to front end

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 
 import LandingPage from './pages/LandingPage';
+import Test from './test/Test';
 // import logo from './logo.svg';
 
 function App() {
   return (
     <div className="App">
       <LandingPage />
+      <Test />
     </div>
   );
 }
--- src/server/webSockets/webSockets.js
+++ src/server/webSockets/webSockets.js
@@ -2,7 +2,7 @@ var express = require('express');
 var socket = require('socket.io');
 
 var app = express();
-var server = app.listen(3000, function(){
+var server = app.listen(3001, function(){
     console.log('listening to request on port 3000');
 });
 
@@ -12,6 +12,6 @@ app.use(express.static('src'));
 //setting up socket
 var io = socket(server);
 
-io.on('connection', function(){
-    console.log('made socket connection');
-})
+io.on('connection', function(socket){
+    console.log('made socket connection', socket.id);
+});
