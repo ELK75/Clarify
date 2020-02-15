dfae264b13f6d514ba2f57ecd1244d2b2f259c81
neel9248 <neeltrivedi10@gmail.com>
2020-02-15 02:04:56 -0500

    changes

--- src/server/webSockets/webSockets.js
+++ src/server/webSockets/webSockets.js
@@ -2,7 +2,7 @@ var express = require('express');
 var socket = require('socket.io');
 
 var app = express();
-var server = app.listen(3001, function(){
+var server = app.listen(4000, function(){
     console.log('listening to request on port 3000');
 });
 
@@ -12,6 +12,14 @@ app.use(express.static('src'));
 //setting up socket
 var io = socket(server);
 
-io.on('connection', function(socket){
+io.on('connection', socket => {
     console.log('made socket connection', socket.id);
+
+   // receiving data sent and emitting to all sockets
+    socket.on('chat', data =>{
+        console.log(data.type);
+        io.sockets.emit('chat', data);
+    })
 });
+
+//server.listen(port);
--- src/test/Test.jsx
+++ src/test/Test.jsx
@@ -1,14 +1,50 @@
-import React from 'react';
+import React, {useState} from 'react';
 
+import Message from '../components/Message';
+
+//make front end connection
 const io = require('socket.io-client');
-const socket = io.connect('http://localhost:3001');
+const socket = io.connect('http://localhost:4000');
 
-export default function test() {
+const Test = () => {
+    const[type, setType]= useState('');
+    const[output, setOutput] = useState('');
+    const [messages, setMessages] = useState([]);
+   
+    const handleNewMessage = (e) => {
+        e.preventDefault();
+        console.log('new message emmitting');
+        if(type){
+            console.log(type);
+            socket.emit('chat', {type: type})
+        }
+    }
+
+    socket.on('chat', data => {
+        console.log(data);
+        setMessages([...messages, <Message text={data.type}/>]);
+    })
+    
+
     return ( 
         <div>
-            <div> hello</div>
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.3.0/socket.io.dev.js"></script>
+             <form onSubmit={handleNewMessage}>
+                <div id= "chat">
+                    <input id = "type" type = "text"  value={type}
+                    onChange={(e)=> setType(e.target.value)}/>
+                    <button type= "submit">send</button>
+                </div>
+                
+            </form>
+            
+            <div>
+                {/* {output} */}
+                {messages}
+            </div>
         </div>
+       
      );
 }
+
+export default Test;
  
