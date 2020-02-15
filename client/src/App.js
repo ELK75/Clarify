9eeaf111aabd35c1717ac1da4c8d036f761ecd54
Kenan Rustamov <kenanrustamov@gmail.com>
2020-02-15 01:31:20 -0500

    REFACTORED EVERYTHING

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,12 @@ import React from 'react';
 import './App.css';
 
 import LandingPage from './pages/LandingPage';
-import Test from './test/Test';
 // import logo from './logo.svg';
 
 function App() {
   return (
     <div className="App">
       <LandingPage />
-      <Test />
     </div>
   );
 }
---
+++ client/src/server/config/keys.js
@@ -0,0 +1,3 @@
+module.exports={
+    mongoURI: 'mongodb+srv://Kenan:<password>@cluster0-bv5bw.gcp.mongodb.net/test?retryWrites=true&w=majority'
+}
